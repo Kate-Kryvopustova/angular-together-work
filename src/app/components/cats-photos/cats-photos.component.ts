63ca8995f7bdfa1aa7a3cc1165c7b30493ff12ee
NataliiaKoval <hakunata@gmail.com>
2021-03-15 11:21:45 +0200

    load images in CatsPhotosComponent

--- src/app/components/cats-photos/cats-photos.component.ts
+++ src/app/components/cats-photos/cats-photos.component.ts
@@ -1,16 +1,30 @@
 import {Component, OnInit} from '@angular/core';
+import {PhotosService} from "../../services/photos.service";
 
 @Component({
   selector: 'app-cats-photos',
   templateUrl: './cats-photos.component.html',
   styleUrls: ['./cats-photos.component.css']
 })
-export class CatsPhotosComponent implements OnInit {
 
-  constructor() {
+export class CatsPhotosComponent implements OnInit {
+  term = 'cats';
+  catsList = [];
+  totalLength: number;
+  page = 1;
+
+  constructor(private photos: PhotosService) {
   }
 
   ngOnInit(): void {
+    this.getPhotos();
   }
 
+  getPhotos(): void {
+    this.photos.search(this.term).subscribe(response => {
+      console.log(response);
+      this.catsList = response.results;
+      this.totalLength = response.results.length;
+    });
+  }
 }
