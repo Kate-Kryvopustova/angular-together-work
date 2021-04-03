9ba623b0fc2fa9b967604872892776b76496a1e9
Igor Kryvopustov <ihorKr@tahometer.com>
2021-04-03 12:00:02 +0300

    additional chages

--- src/app/components/chart/chart.component.ts
+++ src/app/components/chart/chart.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Chart } from 'node_modules/chart.js';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-chart',
