50e71dc7695db51467f838c04f877ca43c81aa51
Igor Kryvopustov <ihorKr@tahometer.com>
2021-04-03 11:46:23 +0300

    delete unused code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+
 import {HomeComponent} from './components/home/home.component';
 import {ChartComponent} from './components/chart/chart.component';
 import {FormComponent} from './components/form/form.component';
